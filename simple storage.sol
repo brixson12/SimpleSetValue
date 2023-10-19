90f0b015bbb5e4d1890d98fa56a3c8d502fbdc0c
brixson12 <141811018+brixson12@users.noreply.github.com>
2023-10-19 10:15:08 +0800

    Add files via upload

---
+++ simple storage.sol
@@ -0,0 +1,16 @@
+
+// SimpleStorage.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SimpleStorage {
+    uint256 public storedData;
+
+    function set(uint256 _value) public {
+        storedData = _value;
+    }
+
+    function get() public view returns (uint256) {
+        return storedData;
+    }
+}
